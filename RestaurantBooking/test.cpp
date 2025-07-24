44f8c23f36336d24288cefe6701b5fff5fe8980e
ies.cho <ies.cho@sec.com>
2025-07-24 13:34:20 +0900

    [feature]p31

--- RestaurantBooking/test.cpp
+++ RestaurantBooking/test.cpp
@@ -1,5 +1,6 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
+#include "testable_sms_sender.cpp"
 
 using namespace testing;
 
@@ -87,8 +88,17 @@ TEST_F(BookingItem, 시간대별인원제한이있다같은시간대가다르면Capacity차있어도스케
 	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
-TEST(BookingSchedulerTest, 예약완료시SMS는무조건발송) {
+TEST_F(BookingItem, 예약완료시SMS는무조건발송) {
+	//arange
+	TestableSmsSender testableSmsSender;
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
+	bookingScheduler.setSmsSender(&testableSmsSender);
 
+	//act
+	bookingScheduler.addSchedule(schedule);
+
+	//assert
+	EXPECT_EQ(true, testableSmsSender.isSendMethodCalled());
 }
 
 TEST(BookingSchedulerTest, 이메일이없는경우에는이메일미발송) {
